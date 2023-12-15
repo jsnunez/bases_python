63e0347276cdd53ad5069ff2899f2d5cdd8e4ab7
joan sebastian nuñez serrano <jsnunez94@gmail.com>
2023-12-14 22:38:03 -0500

    Update notas.py

--- notas.py
+++ notas.py
@@ -1,6 +1,11 @@
 n1=float(input("ingresar nota1:"))
 n2=float(input("ingresar nota2:"))
 nl=float(input("ingresar nota laboratorio:"))
-nf=60
-n3=((nf-(nl*0.3))*3/0.7)-n1-n2
-print(round(n3,3))
+nf=59.5
+x=0
+n3=0
+while nf>=x :
+    n3=n3+1
+    x=(n1+n2+n3)/3*0.7+nl*0.3
+    
+print(n3)
